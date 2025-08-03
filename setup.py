2d2236b5ddb9ffd0fe820700302a610d7529fd9d
dheeraj24797 <dheeraj.sree222@gmail.com>
2025-08-03 15:31:48 -0500

    "Requirements_files"

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "MediWatch_project"
+AUTHOR_USER_NAME = "Dheeraj Sree Sandupatla"
+SRC_REPO = "MediWatch_project"
+AUTHOR_EMAIL = "dheeraj.sree222@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="MediWatch project to determine readmission of patients within 30days of discharge in an app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
