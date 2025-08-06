d937afd00d81998d4272571dbf64ff8c66ad07f9
dheeraj24797 <dheeraj.sree222@gmail.com>
2025-08-05 19:50:14 -0500

    "Data Validation Successfully done"

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from Mediwatch_project import logger
 from Mediwatch_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from Mediwatch_project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
