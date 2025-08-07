a4b896966d45036aeeb464646fe210fa79016794
dheeraj24797 <dheeraj.sree222@gmail.com>
2025-08-06 21:44:24 -0500

    Model training in progress

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 from Mediwatch_project import logger
 from Mediwatch_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Mediwatch_project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-
-
-
-
-
-
-
 from Mediwatch_project.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 
